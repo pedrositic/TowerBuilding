fa02d369b6ca4b4fba194315f93153faba48e5c4
Pau Pedros <paupedros10@gmail.com>
2025-05-13 17:29:30 +0200

    GameOver casi feta

--- core/src/main/java/paupedros/m8/uf3/GameOverScreen.java
+++ core/src/main/java/paupedros/m8/uf3/GameOverScreen.java
@@ -5,11 +5,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 /**
- * Pantalla de fin de partida con fondo, puntuación grande, título GAME OVER y dos botones.
+ * Pantalla de fin de partida con fondo, puntuación centrada, título GAME OVER y dos botones.
  */
 public class GameOverScreen implements Screen {
     private final MainGame game;
@@ -22,30 +23,31 @@ public class GameOverScreen implements Screen {
     private final Rectangle retryBounds;
     private final Rectangle exitBounds;
     private final float buttonScale;
+    private final GlyphLayout layout;
 
     public GameOverScreen(MainGame game, int score) {
         this.game = game;
         this.score = score;
-        // Cargar texturas
+
         background = game.backgroundTextures[0];
         titleTexture = new Texture("Game_over.png");
         scoreFont = new BitmapFont();
-        scoreFont.getData().setScale(4); // tamaño grande para la puntuación
+        scoreFont.getData().setScale(10);
+        layout = new GlyphLayout();
+
         retryButton = new Texture("play_again_button.png");
         exitButton = new Texture("sortir_button.png");
-        // Escala de botones para que sean más grandes
         buttonScale = 1.5f;
-        // Calcular posiciones
+
         float centerX = MainGame.WIDTH / 2f;
-        // Puntuación en la parte superior
-        // Título GAME OVER debajo de la puntuación
         float titleY = MainGame.HEIGHT * 0.6f;
-        // Botones debajo del título
+
         float btnWidth = retryButton.getWidth() * buttonScale;
         float btnHeight = retryButton.getHeight() * buttonScale;
         float startX = centerX - btnWidth / 2f;
         float retryY = titleY - titleTexture.getHeight() - 80;
         float exitY = retryY - btnHeight - 40;
+
         retryBounds = new Rectangle(startX, retryY, btnWidth, btnHeight);
         exitBounds = new Rectangle(startX, exitY, btnWidth, btnHeight);
     }
@@ -54,7 +56,6 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Input
         if (Gdx.input.justTouched()) {
             Vector2 touch = new Vector2(Gdx.input.getX(), Gdx.input.getY());
             touch.y = MainGame.HEIGHT - touch.y;
@@ -67,38 +68,28 @@ public class GameOverScreen implements Screen {
                 return;
             }
         }
-        // Dibujar
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
-        // Fondo completo
+
         game.batch.draw(background, 0, 0, MainGame.WIDTH, MainGame.HEIGHT);
-        // Puntuación centrada grande
+
+        // Puntuación centrada en la pantalla
         String scoreText = String.valueOf(score);
-        float scoreWidth = scoreFont.getSpaceXadvance() * scoreText.length();
-        scoreFont.draw(game.batch,
-            scoreText,
-            (MainGame.WIDTH - scoreWidth) / 2f,
-            MainGame.HEIGHT - 100
-        );
+        layout.setText(scoreFont, scoreText);
+        float scoreX = (MainGame.WIDTH - layout.width) / 2f;
+        float scoreY = (MainGame.HEIGHT + layout.height) / 2f;
+        scoreFont.draw(game.batch, layout, scoreX, scoreY);
+
         // Título GAME OVER
         float titleX = (MainGame.WIDTH - titleTexture.getWidth()) / 2f;
         float titleY = MainGame.HEIGHT * 0.6f;
         game.batch.draw(titleTexture, titleX, titleY);
-        // Botones escalados
-        game.batch.draw(
-            retryButton,
-            retryBounds.x,
-            retryBounds.y,
-            retryBounds.width,
-            retryBounds.height
-        );
-        game.batch.draw(
-            exitButton,
-            exitBounds.x,
-            exitBounds.y,
-            exitBounds.width,
-            exitBounds.height
-        );
+
+        // Botones
+        game.batch.draw(retryButton, retryBounds.x, retryBounds.y, retryBounds.width, retryBounds.height);
+        game.batch.draw(exitButton, exitBounds.x, exitBounds.y, exitBounds.width, exitBounds.height);
+
         game.batch.end();
     }
 
